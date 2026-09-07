4c09f9ad1b950b565c5cf68ace2e50361cbe124c
koi <145122295+userno06@users.noreply.github.com>
2026-09-07 21:09:20 +0800

    Update class.js

--- class.js
+++ class.js
@@ -55,7 +55,7 @@
 
     const students = [{
         alias: 'KOI',
-        image: 'IMAGES/koiii.jpg',
+        image: 'IMAGES/koiii.png',
         role: 'ESCORT',
         description: 'NAME: JHON MICHAEL TRIO\n“May GitHub, walang ka-hug.”'
     }, {
